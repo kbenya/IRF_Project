fc514980e702cac76d2e8a9696be8036b3b7a64a
kbenya <kbb1795@gmail.com>
2020-12-16 20:03:38 +0100

    #2 Meres class létrehozása és a CSV betöltés vizualizációjának hozzáadása

--- BmiCalculator/BmiCalculator/Form1.Designer.cs
+++ BmiCalculator/BmiCalculator/Form1.Designer.cs
@@ -28,13 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.loadButton = new System.Windows.Forms.Button();
+            this.weightDataGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.weightDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // loadButton
+            // 
+            this.loadButton.Location = new System.Drawing.Point(72, 400);
+            this.loadButton.Name = "loadButton";
+            this.loadButton.Size = new System.Drawing.Size(184, 38);
+            this.loadButton.TabIndex = 0;
+            this.loadButton.Text = "Súly adatok betöltése";
+            this.loadButton.UseVisualStyleBackColor = true;
+            // 
+            // weightDataGridView
+            // 
+            this.weightDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.weightDataGridView.Location = new System.Drawing.Point(12, 12);
+            this.weightDataGridView.Name = "weightDataGridView";
+            this.weightDataGridView.RowHeadersWidth = 62;
+            this.weightDataGridView.RowTemplate.Height = 28;
+            this.weightDataGridView.Size = new System.Drawing.Size(295, 355);
+            this.weightDataGridView.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(964, 521);
+            this.Controls.Add(this.weightDataGridView);
+            this.Controls.Add(this.loadButton);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.weightDataGridView)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button loadButton;
+        private System.Windows.Forms.DataGridView weightDataGridView;
     }
 }
 
---
+++ BmiCalculator/BmiCalculator/Meres.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BmiCalculator
+{
+    class Meres
+    {
+        public DateTime MeasurDate { get; set; }
+        public decimal Weight { get; set; }
+    }
+}
